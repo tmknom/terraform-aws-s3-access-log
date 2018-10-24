eb10ff30df44704013dedf4f151e201dc7a80ddc
tmknom <tmk.nom@gmail.com>
2018-10-25 00:40:55 +0900

    Update source ref

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -3,7 +3,7 @@
 # https://www.terraform.io/docs/configuration/modules.html
 
 module "s3_access_log" {
-  source = "git::https://github.com/tmknom/terraform-aws-s3-access-log.git?ref=master"
+  source = "git::https://github.com/tmknom/terraform-aws-s3-access-log.git?ref=tags/v1.0.0"
   name   = "s3-access-log"
 
   versioning_enabled = false
