7c88a6558b638cfe8ee6cf1653fe302f8a25f663
tmknom <tmk.nom@gmail.com>
2018-10-25 01:21:38 +0900

    Add lifecycle rule to complete example

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -9,6 +9,14 @@ module "s3_access_log" {
   versioning_enabled = false
   force_destroy      = true
 
+  lifecycle_rule_enabled                     = true
+  lifecycle_rule_prefix                      = ""
+  standard_ia_transition_days                = "60"
+  glacier_transition_days                    = "90"
+  expiration_days                            = "180"
+  glacier_noncurrent_version_transition_days = "60"
+  noncurrent_version_expiration_days         = "90"
+
   tags = {
     Environment = "prod"
     Name        = "s3-access-log"
